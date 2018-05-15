22435ec26d209bb2ee6056c1e3ba633133074654
Sagar Jain <sagarjain2030@gmail.com>
2018-05-15 21:19:39 +0530

    Feat: LPDS Video 1

---
+++ Learn_Python_for_Data_Science/First_Video.py
@@ -0,0 +1,50 @@
+
+
+#[height,weight,shoes]
+X = [[181, 80, 44],
+     [177, 70, 43],
+     [160, 60, 38],
+     [154, 54, 37],
+     [166, 65, 40],
+     [190, 90, 47],
+     [175, 64, 39],
+     [177, 70, 40],
+     [159, 55, 37],
+     [171, 75, 42],
+     [181, 85, 43]
+    ]
+
+Y = ['male', 'female', 'female', 'female', 'male', 'male', 'male',
+     'female','male','female', 'male']
+
+_X=[[184,84,44],[198,92,48],[183,83,44],[166,47,36],[170,60,38],[172,64,39],[182,80,42],[180,80,43]]
+_Y=['male','male','male','female','female','female','male','male']
+
+#Decision Tree
+from sklearn import tree
+clf_tree = tree.DecisionTreeClassifier()
+clf_tree.fit(X,Y)
+prediction  = clf_tree.predict([[190,70,43]])
+print('Prediction using Decision Tree :', prediction)
+print('score is ', clf_tree.score(_X,_Y)*100)
+
+#Logistic Regression
+from sklearn.linear_model import LogisticRegression
+clf_logisticRegression = LogisticRegression()
+clf_logisticRegression.fit(X,Y)
+print('Prediction using Logistic Regression :', clf_logisticRegression.predict([[190,70,43]]))
+print('score is ', clf_logisticRegression.score(_X,_Y)*100)
+
+#Naive Bayes
+from sklearn.naive_bayes import GaussianNB
+clf_GaussianNB = GaussianNB()
+clf_GaussianNB.fit(X,Y)
+print('Prediction using Naive Bayes :', clf_GaussianNB.predict([[190,70,43]]))
+print('score is ', clf_GaussianNB.score(_X,_Y)*100)
+
+#Support Vector Machine
+from sklearn.svm import SVC
+clf_svm = SVC()
+clf_svm.fit(X,Y)
+print('Prediction using SVM :', clf_svm.predict([[190,70,43]]))
+print('score is ', clf_svm.score(_X,_Y)*100)
