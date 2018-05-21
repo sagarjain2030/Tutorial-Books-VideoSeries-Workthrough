caf7d05fd9ba27fa4124e90915bf76c1bb300a2d
Sagar Jain <sagarjain2030@gmail.com>
2018-05-21 22:31:55 +0530

    Feat: ML mastery Lesson 3

---
+++ ML_MasteryMiniCourse/Lesson_3.py
@@ -0,0 +1,6 @@
+#TODO: Load CSV using Pandas from URL
+import pandas
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv"
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+data = pandas.read_csv(url, names=names)
+print(data.shape)
