de53ea42d3740f3e541c0d37b7cdc6e2ce76ecab
Sagar Jain <sagarjain2030@gmail.com>
2018-05-26 13:58:35 +0530

    Feat: ML Mastery Lesson 7

---
+++ ML_MasteryMiniCourse/Lesson_7.py
@@ -0,0 +1,25 @@
+# Evaluate using Cross Validation
+from pandas import read_csv
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LogisticRegression
+
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv"
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+dataframe = read_csv(url, names=names)
+array = dataframe.values
+
+X = array[:,0:8]
+Y = array[:,8]
+
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.33, random_state=19)
+model = LogisticRegression()
+model.fit(X_train,y_train)
+acc = model.score(X_test,y_test)*100
+print("Accuracy for train and test split : ",acc)
+
+kfold = KFold(n_splits=10, random_state=7)
+model = LogisticRegression()
+results = cross_val_score(model, X, Y, cv=kfold)
+print("Accuracy: %.3f%% (%.3f%%)" % (results.mean()*100.0, results.std()*100.0))
