acb0d9e39686fa825a455e261fd313a86e6dfb4d
sagarjain2030 <sagarjain2030@gmail.com>
2018-10-15 21:29:00 +0530

    Feat: Logistic Regression

---
+++ Tensorflow Tutorial/LogisticRegression.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 15 19:03:37 2018
+
+@author: Sagar
+"""
+
+import tensorflow as tf
+
+mnist = tf.examples.tutorials.mnist.input_data.read_data_sets("/tmp/",one_hot=True)
+
+X = tf.placeholder(tf.float32,[None,28*28])
+Y = tf.placeholder(tf.float32,[None,10])
+
+W = tf.Variable(tf.random_normal([784,10]))
+b = tf.Variable(tf.random_normal([10]))
+
+learnRate = 0.01
+epochNumbers = 50
+batchSize = 128
+
+
+pred = tf.nn.softmax(tf.matmul(X,W) + b)
+cost = tf.reduce_mean(-tf.reduce_sum(tf.multiply(Y,tf.log(pred)),reduction_indices=1))
+optimizer = tf.train.GradientDescentOptimizer(learning_rate = learnRate).minimize(cost)
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+    for epoch in range(epochNumbers):
+        avgCost = 0
+        totalbatch = int(mnist.train.num_examples/batchSize)
+        
+        for i in range(totalbatch):
+            batchX, batchY = mnist.train.next_batch(batchSize)
+            opti = sess.run(optimizer,feed_dict={X:batchX,Y:batchY})
+            c = sess.run(cost,feed_dict={X:batchX,Y:batchY})
+            avgCost += c/totalbatch
+        
+        print("Epoch:"+ str(epoch) + " cost=", str(avgCost))
+    
+    print("Optimiztion Finished")
+    
+    correctPrediction = tf.equal(tf.argmax(pred,1),tf.argmax(Y,1))
+    accuracy = tf.reduce_mean(tf.cast(correctPrediction,tf.float32))
+    print("Accuracy:", accuracy.eval({X: mnist.test.images, Y: mnist.test.labels}))
