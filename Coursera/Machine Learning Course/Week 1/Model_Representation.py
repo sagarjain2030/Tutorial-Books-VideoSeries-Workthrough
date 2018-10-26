5313f7e90fbe3c4c71733e977b9ce5db40079ed7
sagarjain2030 <sagarjain2030@gmail.com>
2018-10-26 23:02:24 +0530

    Feat: Hypothesis value calculator
    Hypothesis considered is h = W0 + W1*X

--- Coursera/Machine Learning Course/Week 1/Model_Representation.py
+++ Coursera/Machine Learning Course/Week 1/Model_Representation.py
@@ -14,23 +14,38 @@ def IODifferentiation(data):
     
     return X,Y
 
-def LinearEquation(X,Y):
-    W0 = np.random.rand(X.shape[0],X.shape[1])
-    W1 = np.random.rand(X.shape[0],X.shape[1])
-    return (W0,W1)
+def LinearEquation(X):
+    Weight = []
+    for i in range(X.shape[0]):
+        W = np.random.rand(X.shape[1]+1)
+        Weight.append(list(W))
+    return Weight
+
+def hypothesisValue(X,weights):
+    Y_pred = []
+    W = np.array(weights)[:,1:]
+    b = np.array(weights)[:,:1]
+    Y_pred.append(np.matmul(W.T,X) + b)
+    return Y_pred
 
 data = [[2104,460],[1416,232],[1534,315],[852,178]]
 
 X,Y = IODifferentiation(data)
-tu = LinearEquation(X,Y)
+Weight = LinearEquation(X)
+
 print(X)
 print(Y)
 print(X.shape)
 print(Y.shape)
 
+print(Weight)
+
+
 print()
 print('************************************************************************')
 print("Y     = W0           +    X   *  W1")
 for i in range(X.shape[0]):
-    print(str(Y[i]) + " = " + str(tu[0][i]) +  " + " + str(X[i]) +  " * "  + str(tu[1][i]))
+    print(str(Y[i]) + " = " + str(Weight[i][0]) +  " + " + str(X[i]) +  " * "  + str(Weight[i][1]))
 print('************************************************************************')
+
+Y_pred = hypothesisValue(X,Weight)
