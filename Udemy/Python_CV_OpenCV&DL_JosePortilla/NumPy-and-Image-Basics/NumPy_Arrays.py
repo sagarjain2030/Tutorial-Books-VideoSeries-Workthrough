a4a1a8d9be22e4859d51610c913088085d0ef07d
sagarjain2030 <sagarjain2030@gmail.com>
2018-12-12 19:45:50 +0530

    Init: Udemy CV course by Jose Portilla

---
+++ Udemy/Python_CV_OpenCV&DL_JosePortilla/NumPy-and-Image-Basics/NumPy_Arrays.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 10 20:22:21 2018
+
+@author: Sagar
+"""
+
+import numpy as np
+mylist = [1,2,3]
+#print(type(mylist))
+
+myArray = np.array(mylist)
+#print(type(myArray))
+
+zeroTo10 = np.arange(0,10)
+#print(zeroTo10)
+
+zeroto10by2 = np.arange(0,10,2)
+#print(zeroto10by2)
+
+zero5 = np.zeros(shape=(5,5))
+#print(zero5)
+
+ones4 = np.ones((2,4))
+print(ones4)
